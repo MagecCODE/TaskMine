6c3bf42684512e0b85146baa367e6f51c5f2b8e4
S@Mu <dj.kiroga@gmail.com>
2025-10-08 21:56:16 +0200

    feat: added environment variable file, routing and visual improvements

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports ={
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "1234",
-    DB: "db_taskmine",
-    dialect: "mysql",
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB_NAME,
+    dialect: process.env.DB_DIALECT,
     pool: {
         max: 5, 
         min: 0,
--- frontend/src/app/add-task/add-task.page.ts
+++ frontend/src/app/add-task/add-task.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-add-task',
@@ -8,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTaskPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+ 
+  goHome() {
+    this.router.navigateByUrl('/home');
+  } 
 }
