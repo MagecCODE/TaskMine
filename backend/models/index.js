227df96454ba3887e29b5b03585e63c0f95bd36b
S@Mu <dj.kiroga@gmail.com>
2025-11-21 10:22:24 +0000

    Fix: general fixed

--- backend/models/index.js
+++ backend/models/index.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
     dbConfig.DB, 
     dbConfig.USER, 
-    dbConfig.PASSWORD,
+    dbConfig.PASSWORD||null,
     {
         host: dbConfig.HOST,
         dialect: dbConfig.dialect,
